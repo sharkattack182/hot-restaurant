badf9be3595c58d5ffc26cd7c58dcf11ec6dd9c3
Matthew Courtney <matthewdc182@gmail.com>
2021-01-22 17:09:13 -0500

    rendered the current tables into the current reservations section on tables.html

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 var express = require("express");
 var path = require("path");
+const { getMaxListeners } = require("process");
 
 var app = express();
 var PORT = 6060;
 
+
+var currentReservations = [
+    {
+        id: 123,
+        name: "Matt Courtney",
+        email: "matt@gmail.com",
+        phone: "607-111-1111"
+    },
+    {
+        id: 321,
+        name: "Kelly Courtney",
+        email: "kelly@gmail.com",
+        phone: "607-222-1111"
+    },
+    {
+        id: 333,
+        name: "Brian Courtney",
+        email: "brian@gmail.com",
+        phone: "607-111-3333"
+    },
+]
+
+// Three main pages
 app.get("/", function(req,res) {
     res.sendFile(path.join(__dirname + "/view.html"))
 });
@@ -14,6 +38,11 @@ app.get("/tables", function(req,res) {
 
 app.get("/reserve", function(req,res) {
     res.sendFile(__dirname + "/reserve.html")
+})
+
+// display all current reservations
+app.get("/api/reservations", function(req,res) {
+    res.json(currentReservations);
 })
 
 app.listen(PORT, function() {
