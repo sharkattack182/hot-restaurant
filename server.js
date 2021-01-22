b4951f94bd85114577c9b948350303040c786649
Matthew Courtney <matthewdc182@gmail.com>
2021-01-22 16:34:23 -0500

    added in script tags and routes for the remaining html pages also added routes to all of the buttons through the app

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ var app = express();
 var PORT = 6060;
 
 app.get("/", function(req,res) {
-    res.sendFile(path.join(__dirname, "view.html"))
+    res.sendFile(path.join(__dirname + "/view.html"))
+});
+
+app.get("/tables", function(req,res) {
+    res.sendFile(__dirname + "/tables.html")
+});
+
+app.get("/reserve", function(req,res) {
+    res.sendFile(__dirname + "/reserve.html")
 })
 
 app.listen(PORT, function() {
